f5f2bb3d230883c6a9edb71a6ae78fc34e4d85f0
grimsi <simon@grimme-online.de>
2021-08-14 20:14:27 +0200

    Updated DB config Updated archetype

--- plugins/archetypes/server-status-plugin-archetype/src/main/resources/archetype-resources/src/main/java/ExampleServerStatusPlugin.java
+++ plugins/archetypes/server-status-plugin-archetype/src/main/resources/archetype-resources/src/main/java/ExampleServerStatusPlugin.java
@@ -1,15 +1,14 @@
 #set($symbol_pound='#')
-        #set($symbol_dollar='$')
-        #set($symbol_escape='\' )
-        package ${package};
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+package ${package};
 
 import de.grimsi.gameradar.pluginapi.PluginMetaInfo;
 import de.grimsi.gameradar.pluginapi.serverstatus.ServerState;
 import de.grimsi.gameradar.pluginapi.serverstatus.ServerStatus;
 import de.grimsi.gameradar.pluginapi.serverstatus.ServerStatusPlugin;
 
-import java.net.URL;
-import java.util.Collections;
+import java.util.List;
 
 public class ExampleServerStatusPlugin implements ServerStatusPlugin {
 
