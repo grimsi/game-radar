605e367f532e266aa5c3036818c1b56bb80085d3
grimsi <simon@grimme-online.de>
2021-09-13 22:59:52 +0200

    Fixed latency simulator sometimes not delaying response

--- backend/src/main/java/de/grimsi/gameradar/backend/dev/LatencyInducingRequestInterceptor.java
+++ backend/src/main/java/de/grimsi/gameradar/backend/dev/LatencyInducingRequestInterceptor.java
@@ -2,10 +2,8 @@ package de.grimsi.gameradar.backend.dev;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,8 +17,9 @@ public class LatencyInducingRequestInterceptor implements HandlerInterceptor {
     private final Duration RESPONSE_DELAY = Duration.ofSeconds(2);
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws InterruptedException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws InterruptedException {
         log.debug("Delaying response for request from '{}' by {} seconds.", request.getRequestURI(), RESPONSE_DELAY.getSeconds());
         Thread.sleep(RESPONSE_DELAY.toMillis());
+        return true;
     }
 }
